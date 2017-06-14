ca66894bd995127eefe7e867f0ba5c4423337178
henriquesw <henriqueswisniewski@gmail.com>
2017-06-14 15:06:42 -0300

    println

--- src/pooatividade3/Caminhao.java
+++ src/pooatividade3/Caminhao.java
@@ -7,6 +7,7 @@ public class Caminhao extends Veiculo {
     public Caminhao(int numeroDeRodas) {
         super.numeroDeRodas = numeroDeRodas;
         super.numeroDePortas = 2;
+        System.out.println("Hello World");
     }
 
     public int getCargaMaxima() {
